439acf38d61e183873c15f3106cc79ecf5ae3242
Olivier Konaté <64969369+0liverFlow@users.noreply.github.com>
2023-05-15 06:56:31 -0700

    Update HookPhish.py

--- HookPhish.py
+++ HookPhish.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
                                                         |
                                                         |
                                                         |
-                Version: 1.0 ~-~-~-~-~-~-~~-~-~-~~-~-~~-|-~~-~-~-~-~-~-~-~-~--~~--~~ Author: 0LIVERFLOW  
+                Version: 1.1 ~-~-~-~-~-~-~~-~-~-~~-~-~~-|-~~-~-~-~-~-~-~-~-~--~~--~~ Author: 0LIVERFLOW  
                                                         |       1
                 ,-.           ,.---'''^\                |                  O        1
                 {   \       ,__\,---'''''`-.,           |      O    O
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     printc(f"\n[bright_blue][*][/bright_blue] Target URL: [red3]{phish_detector.defanged_url}[/red3]\n{'-'*60}\n")
     phish_detector.get_url_redirections(args.verbose)
 
-    if len(phish_detector.expanded_url) > 60:
+    if phish_detector.url != phish_detector.expanded_url and len(phish_detector.expanded_url) > 60:
         printc(f"[spring_green2][+][/spring_green2] Destination url: {phish_detector.expanded_url}")
     # Check Google Safe Browsing Database
     printc(f"\n[bright_blue][*][/bright_blue] Checking Google Safe Browsing Database\n{'-'*42}")
@@ -52,12 +52,19 @@ if __name__ == "__main__":
     # Check if the user specifies the API Key file
     config = configparser.ConfigParser()
     config.read('config/config.ini')
-    abuse_ip_db_api_key, urlscan_io_api_key = config['APIs']['ABUSEIPDB_API_KEY'], config['APIs']['URLSCAN_API_KEY']
+    abuse_ip_db_api_key, urlscan_io_api_key, virustotal_api_key = config['APIs']['ABUSEIPDB_API_KEY'], config['APIs']['URLSCAN_API_KEY'], config['APIs']['VIRUSTOTAL_API_KEY']
+    printc(f"\n[bright_blue][*][/bright_blue] Virustotal.com Reports For [red3]{phish_detector.defanged_url.replace('hxxps[://]', '').replace('hxxp[://]', '')}[/red3]\n{'-'*55}")
+    if virustotal_api_key == "your_virustotal_api_key":
+        printc("[red3][-][/red3] Virustotal.io api's key missing!")
+    else:
+        phish_detector.check_virustotal(phish_detector.expanded_url, virustotal_api_key, args.verbose)
+    time.sleep(2)
     printc(f"\n[bright_blue][*][/bright_blue] Urlscan.io Reports For [red3]{phish_detector.defanged_url.replace('hxxps[://]', '').replace('hxxp[://]', '')}[/red3]\n{'-'*55}")
     if urlscan_io_api_key == "your_urlscan_api_key":
         printc("[red3][-][/red3] Urlscan.io api's key missing!")
     else:
         phish_detector.check_urlscan_io(phish_detector.expanded_url, urlscan_io_api_key, args.verbose)
+    time.sleep(2.5)
     printc(f"\n[bright_blue][*][/bright_blue] IP Abuse DB Reports For [red3]{phish_detector.target_ip_address}[/red3]\n{'-'*49}")
     if abuse_ip_db_api_key == "your_abuseipdb_api_key":
         printc("[red3][-][/red3] Abuse ip db api's key missing!")
@@ -66,6 +73,7 @@ if __name__ == "__main__":
             printc(f"[red3][-][/red3] Unable to resolve {phish_detector.get_domain_name(phish_detector.expanded_url)}")
         else:
             phish_detector.check_abuse_ip_db(phish_detector.target_ip_address, abuse_ip_db_api_key, args.verbose)
+    time.sleep(2)
     # Whois Lookup
     printc(f"\n[bright_blue][*][/bright_blue] Whois Lookup For [red3]{phish_detector.target_ip_address}[/red3]\n{'-'*42}")
     if phish_detector.target_ip_address == "0.0.0.0":
